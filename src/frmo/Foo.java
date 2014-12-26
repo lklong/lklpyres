918bc34abc7887129d2b526d106a5bb7c965f048
lklong <380312239@qq.com>
2014-12-27 00:32:37 +0800

    jj

---
+++ src/frmo/Foo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package frmo;
+
+/**
+ * @author lkl
+ *
+ */
+public class Foo {
+
+	/**
+	 * 
+	 */
+	public Foo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
